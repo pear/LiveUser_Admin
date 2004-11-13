1e1e5544d675b0251468fdb22da016a2c710dbb4
Arnaud Limbourg <arnaud@php.net>
2004-11-13 17:42:55 +0000

    * adding package.php script
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser_Admin/trunk@172600 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ package.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * Script to generate package.xml file
+ *
+ * Taken from PEAR::Log, thanks Jon ;)
+ *
+ * $Id$
+ */
+require_once 'PEAR/PackageFileManager.php';
+require_once 'Console/Getopt.php';
+
+$version = '0.14.0';
+
+$notes = <<<EOT
+EOT;
+
+$description = <<<EOT
+  LiveUser_Admin is meant to be used with the LiveUser package.
+  It is composed of all the classes necessary to administrate
+  data used by LiveUser.
+EOT;
+
+$package = new PEAR_PackageFileManager();
+
+$result = $package->setOptions(array(
+    'package'           => 'LiveUser_Admin',
+    'summary'           => 'User authentication and permission management framework',
+    'description'       => $description,
+    'version'           => $version,
+    'state'             => 'beta',
+    'license'           => 'LGPL',
+    'filelistgenerator' => 'cvs',
+    'ignore'            => array('package.php', 'package.xml', 'TODO', 'DefineGenerator'),
+    'notes'             => $notes,
+    'changelogoldtonew' => false,
+    'simpleoutput'      => true,
+    'baseinstalldir'    => '/LiveUser/Admin',
+    'packagedirectory'  => './',
+    'installexceptions' => array(
+    ),
+    'installas'         => array(
+    ),
+    'exceptions'         => array(
+        'lgpl.txt' => 'doc',
+    ),
+    'dir_roles'         => array('sql'               => 'data',
+                                 'docs'              => 'doc',
+                                 'scripts'           => 'script')
+));
+
+if (PEAR::isError($result)) {
+    echo $result->getMessage();
+}
+
+$package->addMaintainer(
+    'mw21st', 'lead', 'Markus Wolff', 'mw21st@php.net'
+);
+$package->addMaintainer(
+    'arnaud', 'lead', 'Arnaud Limbourg', 'arnaud@php.net'
+);
+$package->addMaintainer(
+    'lsmith', 'lead', 'Lukas Kahwe Smith', 'smith@backendmedia.com'
+);
+$package->addMaintainer(
+    'dufuz', 'developer', 'Helgi Şormar', 'dufuz@php.net'
+);
+
+$package->addDependency('php',       '4.2.0',      'ge',  'php', false);
+$package->addDependency('PEAR',      '1.3.1',      'ge',  'pkg', false);
+$package->addDependency('Log',       '1.7.0',      'ge',  'pkg', true);
+$package->addDependency('DB',        '1.6.0',      'ge',  'pkg', true);
+$package->addDependency('MDB',       '1.1.4',      'ge',  'pkg', true);
+$package->addDependency('MDB2',      '2.0.0beta2', 'ge',  'pkg', true);
+$package->addDependency('XML_Tree',  false,        'has', 'pkg', true);
+$package->addDependency('Crypt_RC4', false,        'has', 'pkg', true);
+
+if (isset($_GET['make'])
+    || (isset($_SERVER['argv'][1])
+        && $_SERVER['argv'][1] == 'make')) {
+    $result = $package->writePackageFile();
+} else {
+    $result = $package->debugPackageFile();
+}
+
+if (PEAR::isError($result)) {
+    echo $result->getMessage();
+}
