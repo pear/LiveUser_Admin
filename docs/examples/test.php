bfbcc2fb53c62c8ba0c52cb0478411b12a66256b
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2004-11-18 17:17:55 +0000

    keep in sync
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser_Admin/trunk@172963 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/test.php
+++ docs/examples/test.php
@@ -2,7 +2,7 @@
 
 require_once 'LiveUser/Admin.php';
 
-$dsn = 'mysql://root:@localhost/liveuser_test';
+$dsn = 'mysql://dufuz:shadiw@localhost/liveuser_test';
 
 $liveuserConfig = array(
     'login' => array(
@@ -17,12 +17,12 @@ $liveuserConfig = array(
         ),
     ),
     'permContainer' => array(
-        'type'  => 'Simple',
+        'type'  => 'Medium',
         'stack' => array('MDB2' => array('dsn' => $dsn, 'prefix'     => 'liveuser_')),
     ),
 );
 
-$lu =& LiveUser_Admin::factory($liveuserConfig, 'de');
+$lu =& LiveUser_Admin::factory($liveuserConfig);
 $lu->setAdminContainers();
 
 $params_rights = array(
@@ -58,11 +58,18 @@ $params_users = array(
     'offset' => 0,
 );
 
+$params_applications = array();
+
+$params_areas = array();
+
+$params_groups = array('filters' => array('perm_user_id' => '1'));
+
+echo '<pre>';
 echo 'input';
 var_dump($params_rights);
 echo '<hr>';
 echo 'output';
-var_dump($lu->perm->getRights($params_rights));
+#var_dump($lu->perm->getRights($params_rights));
 echo '<hr>';
 echo 'underlying query:';
 var_dump($lu->perm->_storage->dbc->last_query);
@@ -71,9 +78,39 @@ echo 'input';
 var_dump($params_users);
 echo '<hr>';
 echo 'output';
-var_dump($lu->perm->getUser($params_users));
+#var_dump($lu->perm->getUser($params_users));
 echo '<hr>';
 echo 'underlying query:';
 var_dump($lu->perm->_storage->dbc->last_query);
 
+echo '<hr>';
+echo 'input';
+var_dump($params_applications);
+echo '<hr>';
+echo 'output';
+var_dump($lu->perm->getApplications($params_applications));
+echo '<hr>';
+echo 'underlying query:';
+var_dump($lu->perm->_storage->dbc->last_query);
+
+echo '<hr>';
+echo 'input';
+var_dump($params_areas);
+echo '<hr>';
+echo 'output';
+var_dump($lu->perm->getAreas($params_areas));
+echo '<hr>';
+echo 'underlying query:';
+var_dump($lu->perm->_storage->dbc->last_query);
+
+echo '<hr>';
+echo 'input';
+var_dump($params_groups);
+echo '<hr>';
+echo 'output';
+var_dump($lu->perm->getGroups($params_groups));
+echo '<hr>';
+echo 'underlying query:';
+var_dump($lu->perm->_storage->dbc->last_query);
+echo '</pre>';
 ?>
