8a47ed7c09796c798534e8e7922fe729cbf52a92
Lukas Smith <lsmith@php.net>
2005-05-06 11:26:55 +0000

    - fixed bug in table aliasing in createSelect()
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser_Admin/trunk@185902 c90b9560-bf6c-de11-be94-00142212c4b1

--- Storage/SQL.php
+++ Storage/SQL.php
@@ -424,12 +424,13 @@ class LiveUser_Admin_Storage_SQL extends LiveUser_Admin_Storage
             $joinfilters = $result[0];
         }
 
+        $tables = array_keys($tables);
         foreach($tables as $key => $table) {
             $tables[$key] = $this->prefix.$this->alias[$table];
         }
         // build SELECT query
         $query = 'SELECT '.implode(', ', $fields);
-        $query.= "\n".' FROM '.implode(', ', array_keys($tables));
+        $query.= "\n".' FROM '.implode(', ', $tables);
         $query.= $this->createWhere($filters, $joinfilters);
         if ($orders) {
             $query.= "\n".' ORDER BY ';
