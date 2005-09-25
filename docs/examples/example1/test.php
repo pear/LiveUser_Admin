50fb72c309f96bfbefb65b4207269820a767eb1f
Lukas Smith <lsmith@php.net>
2005-09-25 08:53:51 +0000

    - cosmetic fix
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser_Admin/trunk@196892 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/example1/test.php
+++ docs/examples/example1/test.php
@@ -8,6 +8,8 @@ $rmArea = $admin->perm->removeArea($filters);
 if ($rmArea === false) {
     echo '<strong>Error on line: '.__LINE__.'</strong><br />';
     print_r($admin->getErrors());
+} elseif ($rmArea === 0) {
+    echo '<strong>Area</strong> to remove did not exist<br />';
 } else {
     echo '<strong>Area</strong> was removed<br />';
 }
