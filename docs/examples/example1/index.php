a80dff99453f9e22c455c3486a4f2f1766a02e3d
Lukas Smith <lsmith@php.net>
2006-04-09 13:12:02 +0000

    - added link to area admin area test
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser_Admin/trunk@211035 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/example1/index.php
+++ docs/examples/example1/index.php
@@ -69,6 +69,7 @@ echo '
 <a href="UserRights.php'.$qstring.'">UserRights</a> |
 <a href="OutputRightsConstants.php'.$qstring.'">OutputRightsConstants</a> |
 <a href="Translation.php'.$qstring.'">Translation</a> |
+<a href="Area_Admin_Areas.php'.$qstring.'">Area Admin Areas</a> |
 <a href="test.php'.$qstring.'">Test</a><br />';
 ?>
 So that these test will run you have to have <a href="http://pear.php.net/package/Var_Dump">Var_Dump</a> installed<br /><br />
