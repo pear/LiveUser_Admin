2e1da35c01f9832212f683ea314ed88edb511382
Lukas Smith <lsmith@php.net>
2006-05-25 10:45:11 +0000

    - updated dep on liveuser
    git-svn-id: http://svn.php.net/repository/pear/packages/LiveUser_Admin/trunk@213719 c90b9560-bf6c-de11-be94-00142212c4b1

--- package.php
+++ package.php
@@ -89,7 +89,7 @@ $package->addMaintainer(
 
 $package->addDependency('php',       '4.2.0', 'ge',  'php', false);
 $package->addDependency('PEAR',      '1.3.1', 'ge',  'pkg', false);
-$package->addDependency('LiveUser','0.16.11', 'ge',  'pkg', false);
+$package->addDependency('LiveUser','0.16.12', 'ge',  'pkg', false);
 $package->addDependency('Log',       '1.7.0', 'ge',  'pkg', true);
 $package->addDependency('DB',        '1.6.0', 'ge',  'pkg', true);
 $package->addDependency('MDB',       '1.1.4', 'ge',  'pkg', true);
